669098ff6e73350a44c7eb160b7631d13df5ca45
Todmy <tolok.d@gmail.com>
2014-07-13 18:29:57 +0300

    add part which search a longest word in the line.

--- src/codebyte_easy/Function.java
+++ src/codebyte_easy/Function.java
@@ -1,5 +1,7 @@
 package codebyte_easy;
 
+import java.util.regex.*;
+
 class Function {
 	String FirstReverse(String str) {
 
@@ -15,14 +17,37 @@ class Function {
 	}
 
 	int FirstFactorial(int num) {
-		
+
 		if (num > 1) {
-			num = (FirstFactorial(num-1)*num);
+			num = (FirstFactorial(num - 1) * num);
 		} else {
 			return num;
 		}
 
 		return num;
 
+	}
+
+	String LongestWord(String line) {
+		Pattern pat = Pattern.compile("[a-zA-Z]");// a-z
+		int num = 0;
+		int numtmp = 0;
+		line += " ";
+		String word = "";
+		for (int i = 0; i < line.length(); i++) {
+			String sign = line.substring(i, i + 1);
+			Matcher mat = pat.matcher(sign);
+			boolean tmp = mat.matches();
+			if (sign.equals(" ") || !tmp) {
+				if (num < numtmp) {
+					word = line.substring(i - numtmp, i);
+					num = numtmp;
+				}
+				numtmp = 0;
+			} else if (tmp) {
+				numtmp++;
+			}
+		}
+		return word;
 	}
 }
--- src/codebyte_easy/TestClass.java
+++ src/codebyte_easy/TestClass.java
@@ -9,14 +9,19 @@ class TestClass {
 		// keep this function call here
 		Scanner s = new Scanner(System.in);
 		Function c = new Function();
-//#1		
-		System.out.println("Enter phrase for reverse: ");
+		// #1
+		System.out.println("Enter a phrase for reverse: ");
 		String tmpString = s.nextLine();
 		System.out.println(c.FirstReverse(tmpString));
-//#2		
-		System.out.println("Enter number for calculation the factorial: ");
+//		// #2
+		System.out.println("Enter a number for calculation the factorial: ");
 		int tmpInt = s.nextInt();
 		System.out.println(c.FirstFactorial(tmpInt));
+		// #3
+		s.nextLine();
+		System.out.println("Enter a String line: ");
+		String tmpLine = s.nextLine();
+		System.out.println(c.LongestWord(tmpLine));
 
 	}
 }
